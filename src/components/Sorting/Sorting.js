76c41a1edb3ca6089be4a2ca7ecb0dfbedf19c41
Ludmila Gubanova <darlingmila@yandex.ru>
2022-12-28 18:37:39 +0300

    fix sorting btns in sorting

--- src/components/Sorting/Sorting.js
+++ src/components/Sorting/Sorting.js
@@ -2,67 +2,110 @@ import "./Sorting.css";
 
 import React from "react";
 
-function Sorting({ isDoneFlag, setIsDoneFlag, isImportantFlag, setIsImportantFlag }) {
+function Sorting({
+  isDoneFlag,
+  setIsDoneFlag,
+  isImportantFlag,
+  setIsImportantFlag
+}) {
   return (
     <section className="sortingSection">
-      <fieldset className="sortingSection__fieldset">
-        <label htmlFor="isDone-true">Сделанные</label>
-        <input
-          type="radio"
-          name="isDone"
-          id="isDone-true"
-          value="true"
-          onChange={(e) => setIsDoneFlag(e.target.value)}
-          checked={isDoneFlag === "true"}
-        />
-        <label htmlFor="isDone-false">Не сделанные</label>
-        <input
-          type="radio"
-          name="isDone"
-          id="isDone-false"
-          value="false"
-          onChange={(e) => setIsDoneFlag(e.target.value)}
-          checked={isDoneFlag === "false"}
-        />
-        <label htmlFor="isDone-all">Все</label>
-        <input
-          type="radio"
-          name="isDone"
-          id="isDone-all"
-          value="isDone-all"
-          onChange={(e) => setIsDoneFlag(e.target.value)}
-          checked={isDoneFlag === "isDone-all"}
-        />
-      </fieldset>
+      <fieldset className="sortingSection__fieldset sortingSection__fieldset_sortingBtns">
+        <div>
+          <input
+            type="radio"
+            name="isDone"
+            id="isDone-true"
+            value="true"
+            onChange={(e) => setIsDoneFlag(e.target.value)}
+            checked={isDoneFlag === "true"}
+          />
+          <label
+            className="sortingSection__label"
+            htmlFor="isDone-true"
+            title="Завершенные"
+          >
+            &#10003;
+          </label>
+          <input
+            type="radio"
+            name="isDone"
+            id="isDone-false"
+            value="false"
+            onChange={(e) => setIsDoneFlag(e.target.value)}
+            checked={isDoneFlag === "false"}
+          />
+          <label
+            className="sortingSection__label"
+            htmlFor="isDone-false"
+            title="Незавершенные"
+          >
+            &#9747;
+          </label>
+          <input
+            type="radio"
+            name="isDone"
+            id="isDone-all"
+            value="isDone-all"
+            onChange={(e) => setIsDoneFlag(e.target.value)}
+            checked={isDoneFlag === "isDone-all"}
+          />
+          <label
+            className="sortingSection__label"
+            htmlFor="isDone-all"
+            title="Все"
+          >
+            Все
+          </label>
+        </div>
 
-      <fieldset className="sortingSection__fieldset">
-        <label htmlFor="isImportant-true">Важные</label>
-        <input
-          type="radio"
-          name="isImportant"
-          id="isImportant-true"
-          value="true"
-          onChange={(e) => setIsImportantFlag(e.target.value)}
-          checked={isImportantFlag === "true"}
-        />
-        <label htmlFor="isImportant-false">Обычные</label>
-        <input
-          type="radio"
-          name="isImportant"
-          id="isImportant-false"
-          value="false"
-          onChange={(e) => setIsImportantFlag(e.target.value)}
-          checked={isImportantFlag === "false"}
-        />
-        <label htmlFor="isImportant-all">Все</label>
-        <input
-          type="radio"
-          name="isImportant"
-          id="isImportant-all"
-          value="isImportant-all"
-          onChange={(e) => setIsImportantFlag(e.target.value)}
-          checked={isImportantFlag === "isImportant-all"}
-        />
+        <div>
+          <input
+            type="radio"
+            name="isImportant"
+            id="isImportant-true"
+            value="true"
+            onChange={(e) => setIsImportantFlag(e.target.value)}
+            checked={isImportantFlag === "true"}
+          />
+          <label
+            className="sortingSection__label"
+            htmlFor="isImportant-true"
+            title="Важные"
+          >
+            &#9733;
+          </label>
+          <input
+            type="radio"
+            name="isImportant"
+            id="isImportant-false"
+            value="false"
+            onChange={(e) => setIsImportantFlag(e.target.value)}
+            checked={isImportantFlag === "false"}
+          />
+          <label
+            className="sortingSection__label"
+            htmlFor="isImportant-false"
+            title="Обычные"
+          >
+            &#9734;
+          </label>
+          <input
+            type="radio"
+            name="isImportant"
+            id="isImportant-all"
+            value="isImportant-all"
+            onChange={(e) => setIsImportantFlag(e.target.value)}
+            checked={isImportantFlag === "isImportant-all"}
+          />
+          <label
+            className="sortingSection__label"
+            htmlFor="isImportant-all"
+            title="Все"
+          >
+            Все
+          </label>
+        </div>
       </fieldset>
 
       <fieldset className="sortingSection__fieldset">
